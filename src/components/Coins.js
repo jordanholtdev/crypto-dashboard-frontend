83c4ed73c9eca61e82565ed68e3c5f3d65a10407
Jordan <jordan.su@protonmail.com>
2022-10-17 16:40:26 -0400

    install react-bootstrap

---
+++ src/components/Coins.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Coins extends React.Component {
+    render() {
+        return (
+            <div>This is a list of coins</div>
+        )
+    }
+}
+
+export default Coins;
