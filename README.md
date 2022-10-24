65a1bf19ce32e8ecd464626d88de7bf174f10e4a
Jordan <jordan.su@protonmail.com>
2022-10-24 19:40:06 -0400

    update footer, update README

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # Crypto Dashboard Project - Front-end
 
+This website allows users to follow crypto trends.
+
 This repository stores the front-end code for the Crypto Dashboard project.
 
+## Functionality
+
+-
+- Website shows most popular crypto currencies based on market cap.
+- This website pulls data from a database that pulls information from the Coingecko API.
+- Every coin has a modal that contains detialed information regarding the coin as well as price trend data rendered via Recharts.
+- There is an example portfolio page containing current holdings, value and activity.
+
+## Technology
+
+- [React](https://reactjs.org/)
+- AWS CLI
+- AWS s3
+- [Chakra-ui](https://chakra-ui.com/)
+- [Recharts](https://recharts.org)
+- [Axios](https://axios-http.com/docs/intro)
+
+## Screenshots
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,13 @@ import { Stack, Text, Link } from "@chakra-ui/react";
 class Footer extends React.Component {
     render() {
         return (
-            <Stack p={10}>
-                <Text>Made with 💖 By <Link href="https://github.com/jordanholtdev" isExternal>Jordan Holt</Link></Text>
+            <Stack p={10} alignItems='center'>
+
+                <Text as='b' fontSize='md'>Crypto Dashboard</Text>
+                <Text>Made with 💖 By <Link href="https:jordanholt.dev" isExternal>Jordan Holt</Link></Text>
+                <Text><Link href="https://github.com/jordanholtdev/crypto-dashboard-frontend" isExternal>GitHub Repo</Link></Text>
+                <Text><Link href="https://www.coingecko.com/" isExternal>Powered by CoinGecko</Link></Text>
+                <Text>Site design © 2022 Jordan Holt</Text>
             </Stack>
         )
     }
